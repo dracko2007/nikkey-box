f26f0c5b04e4e9e51156193995d678cc1f8b09d2
dracko2007 <dracko2007@gmail.com>
2026-06-26 20:37:54 +0900

    fix: remove seção App Gratuito da página inicial

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,22 +4,14 @@ import HeroSection from '@/components/home/HeroSection';
 import FeaturedProducts from '@/components/home/FeaturedProducts';
 import HomeVideos from '@/components/home/HomeVideos';
 import ShippingBanner from '@/components/home/ShippingBanner';
-import AppDownloadSection from '@/components/AppDownloadSection';
-import { useLanguage } from '@/context/LanguageContext';
-import FlagIcon from '@/components/FlagIcon';
 
 const Index: React.FC = () => {
-  const { t } = useLanguage();
-
   return (
     <Layout>
       <HeroSection />
       <FeaturedProducts />
       <HomeVideos />
       <ShippingBanner />
-      <AppDownloadSection />
-
-
     </Layout>
   );
 };
